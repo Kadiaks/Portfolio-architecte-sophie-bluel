932f09eb7f27faa0376b9ca0d6a9ed026fada7d3
Kadiaks <kadiaks@gmail.com>
2023-12-15 10:56:56 +0100

    new modif

--- FrontEnd/scripts/index.js
+++ FrontEnd/scripts/index.js
@@ -1,34 +1,3 @@
-const worksUrl = "http://127.0.0.1:5678/api/works"
-const categoriesUrl = "http://127.0.0.1:5678/api/categories"
-//const usersUrl = "http://127.0.0.1:5678/api/users"
-//127.0.0.1:5678/api/works
-fetch(worksUrl)
-    .then(response => {
-        return response.json()
-    })
-    .then(data => {
-        console.log(data)
-    })
-
-//127.0.0.1:5678/api/categories
-fetch(categoriesUrl)
-    .then(response => {
-        return response.json()
-    })
-    .then(data => {
-        console.log(data)
-    })
-
-//127.0.0.1:5678/api/users
-/*fetch(usersUrl)
-    .then(response => {
-        return response.json()
-    })
-    .then(data => {
-        console.log(data)
-    })
-    */
-
 // étape 1.1 : Récupération des travaux depuis le back-end
 
 function generateWorks(works) {
